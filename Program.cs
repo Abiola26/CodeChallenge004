a11fab904d0721b894c74fb2deb55cbc54b3ce29
HP <HP@BIOTECH>
2022-09-27 13:14:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeChallenge04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target.SumArray();
+        }
+    }
+}
---
+++ Target.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge04
+{
+    class Target
+    {
+        public static void SumArray()
+        {
+            Console.WriteLine("Hello World!");
+            int[] nums = { 2, 4, 5, 7, 11, 15 };
+            int target = 9;
+            int lenght = nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine($"index {i} and {j} is equal to {target}");
+                    }
+                }
+            }
+        }
+    }
+}
